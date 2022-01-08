34d89926c6871a7064656e74a07d5c1a69fddf32
Dmitry Khenkin <dkhenkin@crossriverbank.com>
2022-01-08 15:20:05 -0600

    Removing @everyone from success mentions

--- helium-validator-monitor/discord.py
+++ helium-validator-monitor/discord.py
@@ -7,14 +7,16 @@ log = logger.get_logger(__name__)
 
 load_dotenv() #Check Environment Variables in .env file
 webhook_url = getenv('DISCORD_CHANNEL_WEBHOOK_URL')
-webhook = DiscordWebhook(url=webhook_url) #Discord WebHook URL
 
 SUCCESS_COLOR = '0x22bb33'
 FAILURE_COLOR = '0xbb2124'
 
 def send_status_message(validator_name, validator_explorer, validator_status):
+    webhook = DiscordWebhook(url=webhook_url) #Discord WebHook URL
     embed = None
+
     if validator_status == 'online':
+        webhook.set_content("")
         embed = DiscordEmbed(title=f'{validator_name}', url=validator_explorer, color=SUCCESS_COLOR)
         embed.add_embed_field(name="Status Check", value="ONLINE", inline=False)
     else:
